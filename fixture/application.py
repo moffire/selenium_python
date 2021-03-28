67f5b7a82b4c562e57f533e6bb17be20f6000c65
moffire <alexeydudarev.spb@gmail.com>
2021-03-28 18:37:34 +0300

    update fixtures

--- fixture/application.py
+++ fixture/application.py
@@ -16,6 +16,13 @@ class Application:
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
 
+    def is_valid(self):
+        try:
+            self.driver.current_url
+            return True
+        except:
+            return False
+
     def open_home_page(self):
         driver = self.driver
         driver.get(self.HOME_PAGE)
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,13 +1,9 @@
 from model.contact import Contact
 
 def test_add_contact(app):
-    app.session.login(username='admin', password='secret')
     app.contact.create(Contact(firstname='user', lastname='user_lastname', nickname='nickname'))
     app.open_home_page()
-    app.session.logout()
 
 def test_add_empty_contact(app):
-    app.session.login(username='admin', password='secret')
     app.contact.create(Contact(firstname='', lastname='', nickname=''))
     app.open_home_page()
-    app.session.logout()
